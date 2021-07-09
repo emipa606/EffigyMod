cfbf1a16649f621f0d489335b3b891626c7810f5
emipa606 <emil.offermo@litet.net>
2021-07-09 09:45:12 +0200

    Mod updated for 1.3 and passed autotests

--- Source/Thought_MemoryObservation/Thought_MemoryObservationForEffigy.cs
+++ Source/Thought_MemoryObservation/Thought_MemoryObservationForEffigy.cs
@@ -4,21 +4,22 @@ namespace EffigyMod
 {
     // Token: 0x02000002 RID: 2
     public class Thought_MemoryObservationForEffigy : Thought_MemoryObservation
-	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-		public override bool TryMergeWithExistingMemory(out bool showBubble)
-		{
-			bool result;
-			if (!pawn.IsColonist)
-			{
-				result = base.TryMergeWithExistingMemory(out showBubble);
-			}
-			else
-			{
-				showBubble = false;
-				result = true;
-			}
-			return result;
-		}
-	}
+    {
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        public override bool TryMergeWithExistingMemory(out bool showBubble)
+        {
+            bool result;
+            if (!pawn.IsColonist)
+            {
+                result = base.TryMergeWithExistingMemory(out showBubble);
+            }
+            else
+            {
+                showBubble = false;
+                result = true;
+            }
+
+            return result;
+        }
+    }
 }
--- Source/Thoughts_Memory_Effigies/Building_Effigy.cs
+++ Source/Thoughts_Memory_Effigies/Building_Effigy.cs
@@ -1,17 +1,26 @@
 ﻿using RimWorld;
+using Verse;
 
 namespace EffigyMod
 {
     // Token: 0x02000002 RID: 2
-    public class Building_Effigy : Building_Art, IThoughtGiver
-	{
-		// Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
-		public Thought_Memory GiveObservedThought()
-		{
-			var def = ThoughtDef.Named("Thought_Effigy");
-			var thought_MemoryObservation = ThoughtMaker.MakeThought(def) as Thought_MemoryObservation;
-			thought_MemoryObservation.Target = this;
-			return thought_MemoryObservation;
-		}
-	}
+    public class Building_Effigy : Building_Art, IObservedThoughtGiver
+    {
+        public Thought_Memory GiveObservedThought(Pawn observer)
+        {
+            var thoughtDef = ThoughtDef.Named("Thought_Effigy");
+            if (ThoughtMaker.MakeThought(thoughtDef) is not Thought_MemoryObservation thought_MemoryObservation)
+            {
+                return null;
+            }
+
+            thought_MemoryObservation.Target = this;
+            return thought_MemoryObservation;
+        }
+
+        public HistoryEventDef GiveObservedHistoryEvent(Pawn observer)
+        {
+            return null;
+        }
+    }
 }
